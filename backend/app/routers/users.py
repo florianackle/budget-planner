d47e29a0634de9d5ee941f57c7df8febfc177848
florian <40202349+florianackle@users.noreply.github.com>
2024-10-08 17:06:09 +0200

    :bug: fixed bug with login... guess my laptop was drunk :beers:

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ..schemas import User, UserCreate
-from ..services.user_service import register_user, get_user_by_username
+from ..services.user_service import register_user, get_user_by_username, authenticate_user
 from ..dependencies import get_db
 
 router = APIRouter(
@@ -15,5 +15,12 @@ def register_new_user(user: UserCreate, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
 
-    # create user and his initial budget
     return register_user(db=db, user=user)
+
+@router.post("/login")
+def login_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = authenticate_user(db, username=user.username, password=user.password)
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Invalid username or password")
+    # TODO:  JWT-Token-Logik hinzufügen
+    return {"message": "Login successful"}
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,23 +1,18 @@
 from sqlalchemy.orm import Session
 from .. import models, schemas
 from passlib.context import CryptContext
-from ..services.budget_service import create_budget_for_user
 from fastapi import HTTPException
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
 def get_password_hash(password):
     return pwd_context.hash(password)
-
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
-
 
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = get_password_hash(user.password)
@@ -28,14 +23,15 @@ def create_user(db: Session, user: schemas.UserCreate):
 
     return db_user
 
-
 def register_user(db: Session, user: schemas.UserCreate):
     db_user = create_user(db=db, user=user)
 
-    # create budget for user after user is created
-    try:
-        create_budget_for_user(db=db, user_id=db_user.id)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Budget creation failed")
-
     return db_user
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = get_user_by_username(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
