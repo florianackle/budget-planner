73082aa4ba780a13da8fa73d1c90197247f45116
florian <40202349+florianackle@users.noreply.github.com>
2024-10-08 20:02:58 +0200

    :lipstick: added CustomSnackbar for user friendly responses

---
+++ frontend/src/components/CustomSnackbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Snackbar, Alert } from '@mui/material';
+
+const CustomSnackbar = ({ open, message, severity, onClose }) => {
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={3000}
+      onClose={onClose}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+    >
+      <Alert onClose={onClose} severity={severity} sx={{ width: '100%' }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default CustomSnackbar;
