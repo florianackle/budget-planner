3c624ce79bb83905d9e5c830e918eb4c96559e5d
florian <40202349+florianackle@users.noreply.github.com>
2024-10-14 17:39:20 +0200

    :closed_lock_with_key: updated JWT integration in get_current_user function

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,7 +1,12 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from fastapi.security import HTTPBearer
 from .database import SessionLocal
 from .services.user_service import get_user_by_username
+from .services.jwt import decode_access_token
+from jwt import DecodeError
+
+bearer_scheme = HTTPBearer()
 
 def get_db():
     db = SessionLocal()
@@ -10,8 +15,24 @@ def get_db():
     finally:
         db.close()
 
-def get_current_user(username: str, db: Session = Depends(get_db)):
+def get_current_user(token: str = Depends(bearer_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        # Decode the JWT token to extract the username
+        payload = decode_access_token(token.credentials)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except DecodeError:
+        raise credentials_exception
+
+    # Find the user by the extracted username
     user = get_user_by_username(db, username=username)
     if user is None:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise credentials_exception
     return user
